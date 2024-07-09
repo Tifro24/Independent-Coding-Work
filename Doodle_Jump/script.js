ecb712f7140b6a23e03f774336b0beff8d4e3376
Tifro <elijahduma@hotmail.com>
2024-07-09 16:20:33 +0100

    added platforms

--- Doodle_Jump/script.js
+++ Doodle_Jump/script.js
@@ -1,9 +1,50 @@
 const grid = document.getElementById("grid")
 const char = document.createElement("div")
+let charStartLeft = 40
+let charStartBottom = 100
+let isGameOver = false
+let pformNo = 5
+let platforms = []
 
 function createChar(){
     grid.appendChild(char)
     char.classList.add("character")
+    char.style.left = charStartLeft + 'px'
+    char.style.bottom = charStartBottom + 'px'
+    
 }
 
-createChar()
+class Pform {
+    constructor(newPFormBottom) {
+        this.bottom = newPFormBottom
+        this.left =  Math.random() * 315
+        this.visual = document.createElement('div')
+        
+        const visual = this.visual
+        visual.classList.add("platforms")
+        visual.style.left = this.left + 'px'
+        visual.style.bottom = this.bottom + 'px'
+        grid.appendChild(visual)
+    }
+}
+
+function createPlatforms(){
+    for(let i=0; i<pformNo; i++){
+        let pGap = 600 / pformNo
+        let newPFormBottom = 100 + i * pGap
+        let newPform = new Pform(newPFormBottom)
+        platforms.push(newPform)
+        
+
+    }
+}
+
+function begin(){
+    if(isGameOver == false){
+        createChar()
+        createPlatforms()
+    }
+}
+
+// Will attach button to this and make a menu, maybe add a leaderboard too.
+begin()
