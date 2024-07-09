74b3a191978856c91f830ca95752d54c85e5f74a
Tifro <elijahduma@hotmail.com>
2024-07-09 14:00:05 +0100

    created basic character

--- Doodle_Jump/script.js
+++ Doodle_Jump/script.js
@@ -1,1 +1,9 @@
-body = document.getElementsByName("body")
+const grid = document.getElementById("grid")
+const char = document.createElement("div")
+
+function createChar(){
+    grid.appendChild(char)
+    char.classList.add("character")
+}
+
+createChar()
