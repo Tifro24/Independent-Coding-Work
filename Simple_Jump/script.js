52333d60636f4689e67b8a3e0d5027778791ab72
Tifro <elijahduma@hotmail.com>
2024-07-10 10:43:31 +0100

    added platforms

--- Simple_Jump/script.js
+++ Simple_Jump/script.js
@@ -2,6 +2,9 @@ const box = document.getElementById("box")
 const char = document.createElement("div")
 let charStartLeft = 40
 let charStartBottom = 200
+isGameOver = false
+platformNo = 5
+platforms = []
 
 
 function createChar() {
@@ -12,4 +15,49 @@ function createChar() {
     
 }
 
-createChar()
+class PlatClass {
+    constructor(newPlatBottom){
+        this.bottom = newPlatBottom - 100
+        this.left = Math.random() * 1100
+        this.platReal = document.createElement("div")
+
+
+        const platReal = this.platReal
+        platReal.classList.add("platform")
+        platReal.style.left = this.left + 'px'
+        platReal.style.bottom = this.bottom + 'px'
+        box.appendChild(platReal)
+    }
+}
+
+
+function createPlatforms(){
+    for(let i=0; i<platformNo; i++){
+        let platGap = 800 / platformNo
+        let newPlatBottom = 150 + i *  platGap
+        let newPlatform = new PlatClass(newPlatBottom)
+        platforms.push(newPlatform)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function begin(){
+    if (isGameOver == false){
+        createPlatforms()
+        createChar()
+    }
+}
+
+begin()
